300d60683365dd82ec5107e25dda0e5a5747e5f3
chelis1498 <chelis1498@hotmail.com>
2021-10-25 20:52:40 -0500

    Escaner de QR

--- app/src/main/java/com/example/qrstorage/principal.kt
+++ app/src/main/java/com/example/qrstorage/principal.kt
@@ -1,11 +1,66 @@
 package com.example.qrstorage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.qrstorage.databinding.ActivityMainBinding
+import com.example.qrstorage.databinding.ActivityPrincipalBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.zxing.integration.android.IntentIntegrator as IntentIntegrator1
 
 class principal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPrincipalBinding
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnCerrarSesion.setOnClickListener {
+            signOut()
+        }
+
+        binding.btnQr.setOnClickListener {
+            setUpQr()
+        }
     }
+
+    private fun setUpQr() {
+        IntentIntegrator1(this)
+            .setBeepEnabled(true)
+            .setDesiredBarcodeFormats(IntentIntegrator1.QR_CODE)
+            .setPrompt("Scan Qr")
+            .initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val resultado = IntentIntegrator1.parseActivityResult(requestCode, resultCode, data)
+
+        if (resultado != null){
+            if (resultado.contents !== null){
+                binding.textView2.text = resultado.contents
+            }else{
+                Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/example/qrstorage/regUsuarios.kt
+++ app/src/main/java/com/example/qrstorage/regUsuarios.kt
@@ -1,19 +1,21 @@
 package com.example.qrstorage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import com.example.qrstorage.databinding.ActivityMainBinding
+import android.widget.Toast
 import com.example.qrstorage.databinding.ActivityRegUsuariosBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class regUsuarios : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegUsuariosBinding
 
-    private lateinit var logAuth : FirebaseAuth
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,20 +24,67 @@ class regUsuarios : AppCompatActivity() {
         binding = ActivityRegUsuariosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         val equipo = resources.getStringArray(R.array.equipo)
         val puesto = resources.getStringArray(R.array.puesto)
         val aptEquipo = ArrayAdapter(this, R.layout.list_item, equipo)
         val adpPuesto = ArrayAdapter(this, R.layout.list_item, puesto)
 
 
-        //autCompEquipo.setAdapter(aptEquipo)
 
-        with(binding.selectEquipo){
+        with(binding.selectEquipo) {
             setAdapter(aptEquipo)
         }
 
-        with(binding.selectPuesto){
+        with(binding.selectPuesto) {
             setAdapter(adpPuesto)
         }
+
+        binding.btnRegUsuario.setOnClickListener {
+
+            val correo = binding.correoRegUsuario.text.toString()
+            val contrasena = binding.contraseARegUsuario.text.toString()
+
+
+            when {
+                correo.isEmpty() || contrasena.isEmpty() -> {
+                    Toast.makeText(
+                        baseContext, "Ingrese una cuenta de correo valida.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    Toast.makeText(
+                        baseContext, " Correo: $correo Contraseña: $contrasena",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    registroUsuarios(correo, contrasena)
+                    reload()
+                }
+            }
+        }
+
+    }
+     private fun registroUsuarios(email : String, password : String){
+         auth.createUserWithEmailAndPassword(email, password)
+             .addOnCompleteListener(this) { task ->
+                 if (task.isSuccessful) {
+                     // Sign in success, update UI with the signed-in user's information
+                     Log.d("TAG", "createUserWithEmail:success")
+                     val user = auth.currentUser
+                 } else {
+                     // If sign in fails, display a message to the user.
+                     Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                     Toast.makeText(baseContext, "Authentication failed.",
+                         Toast.LENGTH_SHORT).show()
+
+                 }
+             }
+    }
+
+    private fun reload() {
+        val intent = Intent (this, principal::class.java)
+        this.startActivity(intent)
     }
 }
